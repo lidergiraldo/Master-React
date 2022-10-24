d22a384a704f1e107b127f18aac08e3af541ae42
Liderman Giraldo <lidermangiraldo@hotmail.com>
2022-10-24 15:36:33 -0500

    display component data in view

--- 01-primeros-pasos/src/MiComponente.js
+++ 01-primeros-pasos/src/MiComponente.js
@@ -3,10 +3,28 @@ import React from 'react'
 
 //Función del componente
 function MiComponente() {
+  let nombre = 'Liderman'
+  let apellido = 'Giraldo'
+
+  let equipos = {
+    e1: 'Deportivo Cali',
+    e2: 'Real Madrid'
+  }
+
   return (
     <div className='MiComponente'>
         <hr/>
         <h2>Componente creado</h2>
+        <h3>Datos del usuario</h3>
+        <ul>
+            <li>Nombre: <strong>{nombre+' '+apellido}</strong></li>
+        </ul>
+        <h3>Equipos favoritos</h3>
+        <ul>
+            {/*<li><strong>{JSON.stringify(equipos)}</strong></li>*/}
+            <li><strong>{equipos.e1}</strong></li>
+            <li><strong>{equipos.e2}</strong></li>
+        </ul>
         <p>Este es mi primer componente</p>
         <ul>
             <li>React</li>
