267a76068c33667255395a520a2e52834cc208f4
Liderman Giraldo <lidermangiraldo@hotmail.com>
2022-10-24 14:44:30 -0500

    create components

---
+++ 01-primeros-pasos/src/MiComponente.js
@@ -0,0 +1,12 @@
+//Importar Modulos de React / Dependencias
+import React from 'react'
+
+//Función del componente
+function MiComponente() {
+  return (
+    <p>Este es mi primer componente</p>
+  )
+}
+
+//Export
+export default MiComponente
