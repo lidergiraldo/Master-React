82ebe45e35c65d4888589292c3f4f7565c7cde2e
Liderman Giraldo <lidermangiraldo@hotmail.com>
2022-10-26 17:05:07 -0500

    Double click event

--- 01-primeros-pasos/src/EventosComponente.js
+++ 01-primeros-pasos/src/EventosComponente.js
@@ -4,13 +4,20 @@ export const EventosComponente = () => {
 
   const hasDadoClick = (e, nombre) => {
     alert('Hola ' + nombre + ' Haz dado click al botón')
-  }  
+  } 
+  
+  function hasdadoDobleClick(e){
+    alert('Hola has dado doble click al botón')
+  }
 
   return (
     <div>
         <h1>Eventos en React</h1>
         {/*Evento Click */}
-        <button onClick={e => hasDadoClick(e, 'Liderman')}>Enviar</button>
+        <button onClick={e => hasDadoClick(e, 'Liderman')}>Click</button>
+
+        {/*Evento doble Click */}
+        <button onDoubleClick={hasdadoDobleClick}>Doble Click</button>
     </div>
   )
 }
