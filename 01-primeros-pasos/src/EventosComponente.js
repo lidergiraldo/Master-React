7fe827a8a4c6858392c0c225b4c35c1034524988
Liderman Giraldo <lidermangiraldo@hotmail.com>
2023-04-23 12:07:25 -0500

    Evento Mouse enter y leave

--- 01-primeros-pasos/src/EventosComponente.js
+++ 01-primeros-pasos/src/EventosComponente.js
@@ -6,12 +6,30 @@ export const EventosComponente = () => {
         alert('Haz dado click al botón ' + nombre);
     };
 
+    const handleDoubleClick = (e) => {
+        alert('Haz dado doble click al botón ');
+    };
+
+    const handleHazEntrado = (e, accion) => {
+        alert(`Haz ${accion} a la caja`);
+    };
+
     return (
         <div>
             <h1>Eventos en React</h1>
 
             {/* evento click */}
             <button onClick={e => handleClick(e, "Liderman")}>Dame Click!</button>
+
+            {/* Evento doble click */}
+            <button onDoubleClick={ handleDoubleClick }>Dame doble Click!</button>
+
+            {/* Evento OnMousEnter onMouseLeave */}
+            <div id='caja' onMouseEnter={e => handleHazEntrado(e, 'entrado')}
+                           onMouseLeave={e => handleHazEntrado(e, 'salido') }>
+                Pasa por encima
+            </div>
+
 
         </div>
     )
